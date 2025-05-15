f8ca4cf1d89f54314b1eb8d7ed72f5df395f2891
devarogundade <devarogundade@gmail.com>
2025-05-15 15:46:20 +0100

    added homepage

---
+++ homepage/src/App.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { Application } from '@splinetool/runtime';
+import { onMounted } from 'vue';
+import AppHeader from './components/icons/AppHeader.vue';
+
+onMounted(() => {
+  const canvas = document.getElementById('canvas3d')! as HTMLCanvasElement;
+  const app = new Application(canvas);
+  app.load('/scene.splinecode');
+});
+</script>
+
+<template>
+  <AppHeader />
+
+  <section class="hero_section">
+    <div class="app_width">
+      <div class="hero">
+        <div class="text">
+          <h3>Restake, Earn, and Build Actively Validated Services.</h3>
+          <p>Leverage on SUI and Bitcoin economical security for off-chain verification.</p>
+          <a href="https://beta.deeplayr.xyz" target="_blank"><button>Launch Beta</button></a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <canvas id="canvas3d" class="canvas"></canvas>
+</template>
+
+<style scoped>
+.canvas {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  opacity: 0.3;
+}
+
+.hero {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  min-height: 80vh;
+  padding: 50px 0;
+}
+
+.text {
+  width: 1000px;
+  text-align: center;
+  max-width: 100%;
+}
+
+.text h3 {
+  font-size: 80px;
+  line-height: 60px;
+  color: var(--tx-normal);
+  font-weight: 600;
+}
+
+.text p {
+  margin-top: 30px;
+  font-size: 30px;
+  line-height: 40px;
+  color: var(--tx-semi);
+}
+
+.text button {
+  margin-top: 60px;
+  color: var(--tx-normal);
+  padding: 16px 40px;
+  border-radius: 14px;
+  font-size: 16px;
+  border: 0;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  cursor: pointer;
+  box-shadow: rgba(255, 255, 255, 0.1) -3px -3px 9px inset, rgba(255, 255, 255, 0.15) 0 3px 9px inset, rgba(255, 255, 255, 0.6) 0 1px 1px inset, rgba(0, 0, 0, 0.3) 0 -8px 36px inset, rgba(255, 255, 255, 0.6) 0 1px 5px inset, rgba(0, 0, 0, 0.2) 2px 19px 31px;
+  background-color: var(--accent-green);
+  background-image: radial-gradient(93% 87% at 87% 89%, rgba(0, 0, 0, .23) 0, transparent 86.18%), radial-gradient(66% 66% at 26% 20%, rgba(255, 255, 255, .55) 0, rgba(255, 255, 255, 0) 69.79%, rgba(255, 255, 255, 0) 100%);
+  transition: all 150ms ease-in-out;
+  z-index: 10;
+  position: absolute;
+  left: 50%;
+  top: 600px;
+  transform: translate(-50%, 0);
+}
+
+.text button:hover {
+  filter: brightness(1.05);
+}
+
+.text button:active {
+  transform: scale(.95);
+}
+</style>
---
+++ homepage/src/components/icons/AppHeader.vue
@@ -0,0 +1,118 @@
+<template>
+    <section>
+        <div class="app_width">
+            <header>
+                <div class="logo">
+                    <RouterLink to="/">
+                        <h3>Deep<span>Layr.</span></h3>
+                    </RouterLink>
+                </div>
+
+                <div class="tabs">
+                    <a href="https://github.com/fontlabs/deeplayer" target="_blank">
+                        <button class="tab">GitHub</button>
+                    </a>
+
+                    <a href="/" target="_blank">
+                        <button class="tab">Demo Video</button>
+                    </a>
+
+                    <a href="/" target="_blank">
+                        <button class="tab">Pitch Deck</button>
+                    </a>
+
+                    <a href="https://x.com/deep_layr" target="_blank" rel="noopener noreferrer">
+                        <button class="tab">
+                            <img src="/images/x.png" alt="X">
+                        </button>
+                    </a>
+                </div>
+
+                <div class="actions">
+                    <a href="https://beta.deeplayr.xyz" target="_blank"><button>Restake now</button></a>
+                </div>
+            </header>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+section {
+    position: sticky;
+    top: 0;
+    z-index: 99;
+    background: var(--dark);
+    border-bottom: 1px solid var(--bg-lightest);
+}
+
+header {
+    height: 70px;
+    display: grid;
+    align-items: center;
+    grid-template-columns: auto 1fr auto;
+    gap: 40px;
+}
+
+
+.logo h3 {
+    font-size: 24px;
+    font-weight: 500;
+    color: var(--tx-normal);
+    font-style: italic;
+}
+
+.logo h3 span {
+    color: var(--primary-light);
+    font-style: normal;
+}
+
+.tabs {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+}
+
+.tab {
+    padding: 0 20px;
+    height: 40px;
+    border-radius: 20px;
+    color: var(--tx-semi);
+    background: none;
+    border: 1px solid transparent;
+    cursor: pointer;
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+}
+
+.tab img {
+    width: 16px;
+    height: 16px;
+}
+
+.tab_active {
+    color: var(--tx-normal);
+    background: var(--bg-lighter);
+    border: 1px solid var(--bg-lightest);
+}
+
+.actions {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    gap: 20px;
+}
+
+.actions button {
+    cursor: pointer;
+    background: var(--primary);
+    border: none;
+    color: var(--tx-normal);
+    border-radius: 8px;
+    font-size: 16px;
+    height: 40px;
+    padding: 0 20px;
+}
+</style>
